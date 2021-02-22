31790c343b9c75732bab43dc1d49823908053dfc
Pavel Kocman <kocmanpavel@gmail.com>
2021-02-22 13:41:37 +0100

    Add styled-components + theme wrapper

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,10 @@
+import { ThemeProvider } from "styled-components";
+import { addDecorator } from "@storybook/react";
+import { defaultTheme } from "../src/theme/defaultTheme.js";
+import { withThemes } from "@react-theming/storybook-addon";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+addDecorator(withThemes(ThemeProvider, [defaultTheme]));
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,36 +1,29 @@
-import React from 'react';
-
-import { Button } from './Button';
+import { Button } from "./Button";
+import React from "react";
 
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
 const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  label: "Button",
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  label: "Button",
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+  label: "Button",
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+  label: "Button",
 };
